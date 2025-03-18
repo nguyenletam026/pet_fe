7c21c047b3d682ef28ab85a852744d9e2a6527eb
nguyenletam026 <tamnlse170346@fpt.edu.vn>
2025-03-19 00:37:59 +0700

    Update SignupForm.tsx

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -136,7 +136,7 @@ const SignupForm = () => {
 
       if (response.data.code === 1000) {
         alert("Đăng ký thành công!");
-        navigate("/");
+        navigate("/login");
       } else {
         setError("Đăng ký thất bại. Hãy thử lại!");
       }
@@ -309,7 +309,7 @@ const SignupForm = () => {
 
       {/* Phần phải - Hình ảnh minh họa */}
       <div className="hidden md:flex w-1/2 bg-gray-100 p-6 rounded-lg">
-        <img src="https://marketplace.canva.com/EAFJ1a_Jkq4/1/0/1600w/canva-brown-black-simple-modern-pet-shop-logo-3HtOB_YFCeQ.jpg" alt="PetStore Illustration" className="w-full h-full object-cover rounded-lg" />
+        <img src='https://marketplace.canva.com/EAFJ1a_Jkq4/1/0/1600w/canva-brown-black-simple-modern-pet-shop-logo-3HtOB_YFCeQ.jpg' alt="PetStore Illustration" className="w-full h-full object-cover rounded-lg" />
       </div>
     </div>
   );
