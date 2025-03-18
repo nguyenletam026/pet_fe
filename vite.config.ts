41c0fc34c5b95589a21815b168c2aee8f56b0a8d
nguyenletam026 <tamnlse170346@fpt.edu.vn>
2025-03-18 19:27:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
     host: true,  // Cho phép truy cập từ các host bên ngoài
     strictPort: false,
     allowedHosts: [
-      "6ee2-2402-800-63b6-8bec-981e-192e-3aa6-cd7d.ngrok-free.app" // ✅ Thêm ngrok vào danh sách cho phép
+      "540c-2a09-bac1-7ac0-10-00-279-8f.ngrok-free.app" // ✅ Thêm ngrok vào danh sách cho phép
     ],
     proxy: {
       "/api": {
-        target: "https://6ee2-2402-800-63b6-8bec-981e-192e-3aa6-cd7d.ngrok-free.app",
+        target: "https://540c-2a09-bac1-7ac0-10-00-279-8f.ngrok-free.app",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
