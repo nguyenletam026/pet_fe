226c8094e6d592db1e5fd34b566be55336806bd6
nguyenletam026 <tamnlse170346@fpt.edu.vn>
2025-04-12 14:54:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
     host: true,  // Cho phép truy cập từ các host bên ngoài
     strictPort: false,
     allowedHosts: [
-      "pet-bdskewgt0-tams-projects-a4dbd6d6.vercel.app/" // ✅ Thêm ngrok vào danh sách cho phép
+      "pet-bdskewgt0-tams-projects-a4dbd6d6.vercel.app" // ✅ Thêm ngrok vào danh sách cho phép
     ],
     proxy: {
       "/api": {
-        target: "https://pet-bdskewgt0-tams-projects-a4dbd6d6.vercel.app/",
+        target: "https://pet-bdskewgt0-tams-projects-a4dbd6d6.vercel.app",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
