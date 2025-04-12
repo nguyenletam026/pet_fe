5d46b16189aed8a7995808b823b7373b21fb8109
nguyenletam026 <tamnlse170346@fpt.edu.vn>
2025-04-12 14:54:07 +0700

    ấda

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
     host: true,  // Cho phép truy cập từ các host bên ngoài
     strictPort: false,
     allowedHosts: [
-      "540c-2a09-bac1-7ac0-10-00-279-8f.ngrok-free.app" // ✅ Thêm ngrok vào danh sách cho phép
+      "pet-bdskewgt0-tams-projects-a4dbd6d6.vercel.app/" // ✅ Thêm ngrok vào danh sách cho phép
     ],
     proxy: {
       "/api": {
-        target: "https://540c-2a09-bac1-7ac0-10-00-279-8f.ngrok-free.app",
+        target: "https://pet-bdskewgt0-tams-projects-a4dbd6d6.vercel.app/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
