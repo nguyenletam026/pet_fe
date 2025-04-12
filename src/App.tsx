28cb228909cf66b8501b82161913d0401d4ede62
nguyenletam026 <tamnlse170346@fpt.edu.vn>
2025-04-12 15:58:51 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import CartPage from './components/Cart';
 import OrderHistoryPage from './components/OrderHistoryPage';
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/pet_fe">
       <Routes>
         <Route path="/cart" element={<CartPage />} />
         <Route path="/" element={<LoginForm />} />
